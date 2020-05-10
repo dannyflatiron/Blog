77ace12555429f5a03bc73702082bf56fd7f1e8c
dannyflatiron <dannyflatiron@gmail.com>
2020-05-09 21:57:20 -0400

    create action session login & condition validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,11 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(username: params[:user][:username])
         if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
             else
+            flash[:message] = "Invalid information. Please try again"
+            redirect_to '/login'
         end
     end
 
