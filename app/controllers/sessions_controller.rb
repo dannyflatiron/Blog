cfcd0aad2f20c4027f7c46177d77e567b5721c12
dannyflatiron <dannyflatiron@gmail.com>
2020-05-09 21:28:13 -0400

    Fix links on sessions home view. Create action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,15 @@
 class SessionsController < ApplicationController
-
-    def home
+   
+    def create
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            else
+        end
     end
 
     def destroy
+        session.clear
+        redirect_to '/'
     end
+
 end
