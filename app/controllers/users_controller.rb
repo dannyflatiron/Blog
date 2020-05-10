f2bfff44d9fc54b1e9d701b9ca1c31d6cd0c0336
dannyflatiron <dannyflatiron@gmail.com>
2020-05-09 20:08:41 -0400

    If condition create action. Session login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
     end
 
     def create
-        binding.pry
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            render :new
+        end
     end
 
     def show
@@ -14,6 +20,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        param.required(:user).permit(:username, :email, :password)
+        params.required(:user).permit(:username, :email, :password)
     end
 end
