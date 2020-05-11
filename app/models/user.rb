ec16fb7a29f845a97b728782f02f93f44b4e98d5
dannyflatiron <dannyflatiron@gmail.com>
2020-05-11 17:50:15 -0400

    Commented has_mnay categories through posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ApplicationRecord
     has_many :posts, dependent: :destroy
     has_many :comments, dependent: :destroy
     has_many :commented_posts, through: :comments, source: :post, dependent: :destroy
-    # has_many :categories, through :posts
+    # has_many :categories, through: :posts
 
     has_secure_password 
     #gives access to authenticate method, built in validations such as validate password, password confirmation
+
+
 
     def delete_user(user)
         # enable user to delete their account
