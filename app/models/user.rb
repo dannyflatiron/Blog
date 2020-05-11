4a01115ba4733568d5fbb56bc7d1e3258e40af53
dannyflatiron <dannyflatiron@gmail.com>
2020-05-11 15:52:10 -0400

    delete_user method not finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    has_many :commented_posts, through: :comments, source: :post
+    has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    has_many :commented_posts, through: :comments, source: :post, dependent: :destroy
     # has_many :categories, through :posts
 
     has_secure_password 
     #gives access to authenticate method, built in validations such as validate password, password confirmation
 
-
+    def delete_user(user)
+        # enable user to delete their account
+        # how to find user?
+        # scope method? pr findy_by?
+        # user = User.find_by_id()
+    end
 end
