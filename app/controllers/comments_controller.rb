4a6bffb328e838167f7b83973765cdb815b81de4
dannyflatiron <dannyflatiron@gmail.com>
2020-05-10 20:31:32 -0400

    index new create actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+    def index
+        @comments = Comment.all
+    end
+
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+            redirect_to comments_path
+        else
+            render :new
+        end
+
+    end
 end
