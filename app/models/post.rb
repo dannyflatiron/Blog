9381ada128013bb5c1a2a9cc26eb69d259b0ffa9
dannyflatiron <dannyflatiron@gmail.com>
2020-05-11 16:33:48 -0400

    Created two scope methods. Alphabetiza post titles and organize posts in descending order for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :users, through: :comments
+
+  # orders posts title alphabetically
+  scope :organize, -> { order(:title)}
+
+  # orders posts by most comments in descending order
+  scope :most_comments, -> {joins(:comments).group('posts.id').order('count(posts.id) desc')}
 end
