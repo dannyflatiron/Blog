5472eaf914d0552c870ffba1aeff5a1bc1b87154
dannyflatiron <dannyflatiron@gmail.com>
2020-05-11 20:35:33 -0400

    Refactoring

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+
+    def index_display_header
+        if @user 
+            content_tag(:h1, "#{@user.username}'s Posts:")
+        else 
+            content_tag(:h2, "All Posts")
+        end 
+    end
+
 end
