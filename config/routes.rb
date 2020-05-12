a4f15d50357798c41579696c9f93dac8cf741bc7
dannyflatiron <dannyflatiron@gmail.com>
2020-05-11 20:59:59 -0400

    nested comments under users only index action

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :users do
     # /users/id/posts 
     resources :posts, only: [:new, :create, :index]
+    resources :comments, only: [:index]
   end
 
   
