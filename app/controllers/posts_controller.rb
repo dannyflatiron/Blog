8ae03319e828bf7665abd3d3838ed75c3e6ca293
dannyflatiron <dannyflatiron@gmail.com>
2020-05-11 20:35:14 -0400

    Refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
     before_action :redirect_if_not_logged_in
+    before_action :set_post, only: [:show, :edit, :update]
+    before_action :redirect_if_not_post, only: [:show, :edit, :update]
 
     def index
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
@@ -14,6 +16,9 @@ class PostsController < ApplicationController
         @post = Post.new
     end
 
+    def show
+    end
+
     def create
         @post = current_user.posts.build(post_params)
         if @post.save
@@ -23,14 +28,34 @@ class PostsController < ApplicationController
         end
     end
 
-    def show
-        @post = Post.find_by(id: params[:id])
+    def edit
     end
+
+    def update
+        if @post.update(post_params)
+            redirect_to post_path(@post)
+        else
+            render :edit
+        end
+    end
+
 
 
     private 
 
     def post_params
         params.require(:post).permit(:title, :content)
+    end
+
+    def set_post
+        @post = Post.find_by(id: params[:id])
+        if !@post
+            flash[:message] = "Post was not found"
+            redirect_to posts_path
+        end
+    end
+
+    def redirect_if_not_post
+        redirect_to posts_path if !@post || @post.user != current_user
     end
 end
